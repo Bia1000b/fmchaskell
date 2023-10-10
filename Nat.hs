516b858e016708033ad875e6e1d68510d387e0aa
BiancaM <biancamm0107@gmail.com>
2023-10-10 00:48:26 -0300

    first commit

---
+++ Nat.hs
@@ -0,0 +1,19 @@
+module Nat where
+
+import Prelude hiding
+    ((+), (-), (*), (^))
+
+data Nat = O | S Nat
+    deriving ( Eq , Show )
+
+(+) :: Nat -> Nat -> Nat
+(+) n O = n
+(+) n (S m) = S (n + m)
+
+(*) :: Nat -> Nat -> Nat
+(*) n O = O
+(*) n (S m) = n + (n * m)
+
+(^) :: Nat -> Nat -> Nat
+(^) n O = S O
+(^) n (S m) = n * (n ^ m)
