15f23d6bd64022c3547ff0b86c8675ee70e19781
BiancaM <biancamm0107@gmail.com>
2023-12-16 23:57:21 -0300

    muitas atividades

--- Nat.hs
+++ Nat.hs
@@ -1,10 +1,8 @@
 module Nat where
 
-import Prelude hiding
-    ((+), (-), (*), (^))
+import Prelude hiding ((<=), (<), (+), (*), (^), fib, quot, min, gcd, lcm, div, max, pred, rem, (-), if_then_else, leq, eq, ev, od, isMul3, divides)
+import Ordering
 
-data Nat = O | S Nat
-    deriving ( Eq , Show )
 
 (+) :: Nat -> Nat -> Nat
 (+) n O = n
@@ -17,3 +15,28 @@ data Nat = O | S Nat
 (^) :: Nat -> Nat -> Nat
 (^) n O = S O
 (^) n (S m) = n * (n ^ m)
+
+ev :: Nat -> Bool
+ev O = True
+ev (S O) = False
+ev (S (S n)) = ev n
+
+od :: Nat -> Bool
+od O = False
+od (S O) = True
+od (S (S n)) = od n
+
+fib :: Nat -> Nat
+fib O = O
+fib (S O) = S O
+fib (S(S n)) = fib (S n) + fib n
+
+min :: Nat -> Nat -> Nat
+min n O = O
+min O m = O
+min (S n) (S m) = S (min n m)
+
+max :: Nat -> Nat -> Nat
+max n O = n
+max O m = m
+max (S n) (S m) = S(max n m)
